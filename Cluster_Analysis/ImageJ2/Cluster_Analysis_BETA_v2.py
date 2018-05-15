24ce0d9566f662ea323350728b7b3ef53b1623cc
dcolam <david.colameo@uzh.ch>
2018-05-15 15:25:31 +0200

    Update Cluster_Analysis_BETA_v2.py

--- Cluster_Analysis/ImageJ2/Cluster_Analysis_BETA_v2.py
+++ Cluster_Analysis/ImageJ2/Cluster_Analysis_BETA_v2.py
@@ -35,8 +35,8 @@ if not headless:
 else:
 #	dir_path = os.path.realpath('__file__')
 	#print dir_path, os.path.dirname(os.path.realpath('__file__'))
-	dir_path = "/Users/david/Fiji.app"
-	dir_path = os.path.join(dir_path, "plugins", "Cluster_Analysis")
+	dir_path = os.path.dirname(os.path.realpath('__file__'))
+	dir_path = os.path.join(dir_path, "Fiji.app", "plugins", "Cluster_Analysis")
 
 	
 class config(object):
